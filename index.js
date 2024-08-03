bf063508c74ed044da5223198f5f0590e1d14e5d
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-08-04 01:09:59 +0500

    add

---
+++ index.js
@@ -0,0 +1,6 @@
+//                           loading code start        
+const loader = document.getElementById("loader-main-div");
+window.addEventListener("load", function () {
+    loader.style.display = "none"
+})
+//                           loading code ends            
